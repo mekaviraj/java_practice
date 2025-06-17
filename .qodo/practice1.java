9971e8766cbccfe159524fc6ed7f405d4e2c21b4
mekaviraj <mekaviraj1749@gmail.com>
2025-06-18 01:06:09 +0530

    datatypes and methods

---
+++ .qodo/practice1.java
@@ -0,0 +1,12 @@
+
+public class practice1 {
+    public static void main(String[] args) {
+        
+        show();
+
+    }
+    public static void show(){
+        System.out.println("viraj");
+    }
+    
+}
---
+++ DataTypes.java
@@ -0,0 +1,18 @@
+// data assiociated with operations are abstract data types
+// we have str int float boolean byte char long short 
+public class DataTypes {
+    public static void main(String[] args) {
+        // Primitive data types
+        int a = 10 ;
+        char b = 'a';
+        boolean c = true ; 
+        float f = 2.4f;
+        double d = 4.55;
+        long l = 334890L;
+        short s = 12;
+        byte by = 100; // 1 nundi 127 varaku
+        System.out.println(d + " " + a);
+         
+        }
+    }
+
